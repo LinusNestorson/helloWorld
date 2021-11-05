52fd908f941ce082f824846ce7252222fd1773cb
LinNes <lnestorson@gmail.com>
2021-11-05 14:07:52 +0100

    Added another branch

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace helloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World2!");
+            Console.WriteLine("We added another branch!");
         }
     }
 }
